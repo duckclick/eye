9656a351ef364ff7e6857877b2f2d85252130e49
Túlio Ornelas <tulio.ornelas@klarna.com>
2017-01-22 18:35:00 +0100

    only track changes in the viewport

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import jsBase64 from 'js-base64'
 import throttle from 'lodash.throttle'
 
 const THROTTLE_WAIT = 150
+const THRESHOLD = 0
+
 let currentMarkup = null
+let screenWidth = null
+let screenHeight = null
 
 const getCurrentHTML = () =>
   document.getElementsByTagName('html')[0].outerHTML
@@ -25,9 +29,26 @@ const publishMarkup = (markup) => {
     .catch((response) => console.error('quack!', response.data()))
 }
 
+const isInTheViewport = (element) => {
+  const rect = element.getBoundingClientRect()
+  const x1 = -THRESHOLD
+  const y1 = -THRESHOLD
+  const y2 = screenHeight - y1
+  const x2 = screenWidth - x1
+
+  return (
+    (rect.top >= y1 && rect.top <= y2 || rect.bottom >= y1 && rect.bottom <= y2) &&
+    (rect.left >= x1 && rect.left <= x2 || rect.right >= x1 && rect.right <= x2)
+  )
+}
+
 const listenTo = (events) => {
   events.forEach((event) => {
-    document.addEventListener(event, throttle(() => {
+    document.addEventListener(event, throttle((e) => {
+      if (!isInTheViewport(e.target)) {
+        return
+      }
+
       publishMarkup(getCurrentHTML())
     }, THROTTLE_WAIT), false)
   })
@@ -40,6 +61,9 @@ const boot = () => {
   }
 
   loaded = true
+  screenHeight = window.innerHeight || document.documentElement.clientHeight
+  screenWidth = window.innerWidth || document.documentElement.clientWidth
+
   listenTo(['DOMNodeInserted', 'DOMNodeRemoved'])
   publishMarkup(getCurrentHTML())
 }
